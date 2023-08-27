03e04e3b7d647f10c3ed7acedf4b8ffb03aefe51
Shwetha Tinnium Raju <stinnium@alumni.cmu.edu>
2023-08-27 17:12:29 +0000

    Added GPT-2 Streamlit chatbot application

---
+++ gpt2_app.py
@@ -0,0 +1,15 @@
+
+import streamlit as st
+import gpt_2_simple as gpt2
+
+# We assume the model is already loaded in the environment when this app runs.
+# Thus, no explicit loading happens in this code.
+
+st.title("HelpBot")
+
+user_input = st.text_input("You:", "Type your message here...")
+
+if st.button("Reply"):
+    # Assuming the 'sess' variable is available globally
+    response = gpt2.generate(sess, model_name="124M", prefix=user_input, length=100, return_as_list=True)[0]
+    st.text_area("HelpBot:", value=response, height=200)
