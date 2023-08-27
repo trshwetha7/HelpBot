aeae388e7e9aad8d3e2d4a536d42508c108ffe25
Shwetha Tinnium Raju <80608207+trshwetha7@users.noreply.github.com>
2023-08-27 17:51:34 -0400

    Update gpt2_app.py

--- gpt2_app.py
+++ gpt2_app.py
@@ -1,13 +1,23 @@
-
 import streamlit as st
-import gpt_2_simple as gpt2
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
+model_name = "gpt2-medium"
+model = GPT2LMHeadModel.from_pretrained(model_name)
+tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 
 st.title("HelpBot")
 
 user_input = st.text_input("You:", "Type your message here...")
 
 if st.button("Reply"):
-    # Assuming the 'sess' variable is available globally
-    response = gpt2.generate(sess, model_name="124M", prefix=user_input, length=100, return_as_list=True)[0]
+    # Encode the input text
+    input_ids = tokenizer.encode(user_input, return_tensors="pt", max_length=512, truncation=True)
+    
+    # Generate a response from the model
+    output = model.generate(input_ids, max_length=200, pad_token_id=tokenizer.eos_token_id, num_return_sequences=1)
+    
+    # Decode the response
+    response = tokenizer.decode(output[0], skip_special_tokens=True)
+    
     st.text_area("HelpBot:", value=response, height=200)
+
