7dd2ff6f26ec4def989e96d8fc1cf2546ec90a9b
Shwetha Tinnium Raju <80608207+trshwetha7@users.noreply.github.com>
2023-08-27 18:50:59 -0400

    Update gpt2_app.py

--- gpt2_app.py
+++ gpt2_app.py
@@ -5,17 +5,28 @@ from transformers import GPT2LMHeadModel, GPT2Tokenizer
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2-medium")
 model = GPT2LMHeadModel.from_pretrained("gpt2-medium")
 
-st.title("Advanced ChatBot using GPT-2")
+st.title("HelpBot")
 
-user_input = st.text_input("You:", "Type your message here...")
+user_input = st.text_input("You:", placeholder="Type your message here...")
+
 
 if st.button("Reply"):
     # Encode and process the user input
     input_ids = tokenizer.encode(user_input, return_tensors='pt')
     
-    # Generate a response from the model
-    output = model.generate(input_ids, max_length=150, num_return_sequences=1, pad_token_id=tokenizer.eos_token_id)
+    # Generate a response from the model with top-k and top-p sampling
+    output = model.generate(
+        input_ids, 
+        max_length=150, 
+        num_return_sequences=1, 
+        pad_token_id=tokenizer.eos_token_id,
+        top_k=50, 
+        top_p=0.95, 
+        temperature=0.7
+    )
+    
     response = tokenizer.decode(output[0], skip_special_tokens=True)
     
     st.text_area("ChatBot:", value=response, height=200)
+
 
