1d3d102827d67d2cbaac345910516d6efbedf9d8
Shwetha Tinnium Raju <80608207+trshwetha7@users.noreply.github.com>
2023-08-27 18:03:01 -0400

    Update gpt2_app.py

--- gpt2_app.py
+++ gpt2_app.py
@@ -1,23 +1,16 @@
 import streamlit as st
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
-
-model_name = "gpt2-medium"
-model = GPT2LMHeadModel.from_pretrained(model_name)
-tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
 st.title("HelpBot")
+
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2-medium")
+model = GPT2LMHeadModel.from_pretrained("gpt2-medium")
 
 user_input = st.text_input("You:", "Type your message here...")
 
 if st.button("Reply"):
-    # Encode the input text
-    input_ids = tokenizer.encode(user_input, return_tensors="pt", max_length=512, truncation=True)
-    
-    # Generate a response from the model
-    output = model.generate(input_ids, max_length=200, pad_token_id=tokenizer.eos_token_id, num_return_sequences=1)
-    
-    # Decode the response
-    response = tokenizer.decode(output[0], skip_special_tokens=True)
-    
-    st.text_area("HelpBot:", value=response, height=200)
+    input_ids = tokenizer.encode(user_input, return_tensors='pt')
+    output = model.generate(input_ids, max_length=100, num_return_sequences=1)
+    output_text = tokenizer.decode(output[0], skip_special_tokens=True)
+    st.text_area("HelpBot:", value=output_text, height=200)
 
